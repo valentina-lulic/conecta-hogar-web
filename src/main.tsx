f3f43e07aa0463d772a858ade75808b2c5a5d8a9
Denisse <denisse.lh96@gmail.com>
2026-07-25 19:43:27 -0400

    fix: ajuste de router en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; 
 import App from "./App.tsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter> 
-    <App />
-  </BrowserRouter>
+  <App />
 );
